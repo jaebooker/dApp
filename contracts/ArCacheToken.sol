b8ed03898c80838cc165dca4e281becd36bf1c86
jaebooker <jaesonbooker@gmail.com>
2019-06-22 23:17:33 -0700

    cleanup

--- contracts/ArCacheToken.sol
+++ contracts/ArCacheToken.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-import './node_modules/openzeppelin-solidity/contracts/token/ERC20Mintable.sol';
+import './node_modules/openzeppelin-solidity/contracts/token/ERC721Mintable.sol';
 
 contract ArCacheToken is MintableToken {
   string public name = "ArCache Token";
