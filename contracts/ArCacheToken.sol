b331ed7a82eda886ef4e2aa3fbb9e26d5e11ee54
jaebooker <jaesonbooker@gmail.com>
2019-06-29 15:13:50 -0700

    have basic token

--- contracts/ArCacheToken.sol
+++ contracts/ArCacheToken.sol
@@ -1,8 +1,24 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-import './node_modules/openzeppelin-solidity/contracts/token/ERC721Mintable.sol';
+import './node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
 
-contract ArCacheToken is MintableToken {
-  string public name = "ArCache Token";
-  string public symbol = "ARCT";
+contract ArCacheToken is ERC721Token {
+  constructor() ERC721("ArCache Token", "ARCT") public {}
+
+  struct Object {
+    uint8 texture;
+    uint8 type;
+  }
+
+  Object[] objects;
+
+  function mint() public {
+    Object memory _object = Object(uint8(now), uint8(now-1000));
+    uint _id = objects.push(_object) - 1;
+    _mint(msg.sender, _id);
+  }
+
+  function getColorFromId(uint id) public view returns(uint8, uint8) {
+    return (objects[id].texture, objects[id].type);
+  }
 }
---
+++ contracts/CacheFound.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract CacheFound {
+    uint address = 0
+    if (msg.sender) {
+        //do stuff
+    }
+}
