8d2d650f556b90d438e14cdc8be0463ce0fe0cfc
jaebooker <jaesonbooker@gmail.com>
2019-06-29 18:01:33 -0700

    adding to token

--- contracts/ArCacheToken.sol
+++ contracts/ArCacheToken.sol
@@ -15,7 +15,13 @@ contract ArCacheToken is ERC721Token {
   function mint() public {
     Object memory _object = Object(uint8(now), uint8(now-1000));
     uint _id = objects.push(_object) - 1;
-    _mint(msg.sender, _id);
+    _mint(address(0), _id);
+    transferFrom(address(0), msg.sender, tokenId)
+  }
+
+  function transferFrom(address from, address to, uint256 tokenId) public {
+      
+      _transferFrom(address from, address to, uint256 tokenId)
   }
 
   function getObjectVisualFromId(uint id) public view returns(uint8, uint8) {
